6fab911b9f35467f8ac14380f57003df41e64016
EvilCalf <xujiading1997@163.com>
2022-08-18 10:13:19 +0800

    update

--- voc_annotation.py
+++ voc_annotation.py
@@ -12,15 +12,15 @@ classes_path        = 'model_data/SA-HP.txt'
 trainval_percent    = 0.8
 train_percent       = 0.8
 
-VOCdevkit_path  = 'VOCdevkit'
+data_path  = 'VOCdevkit'
 
-VOCdevkit_sets  = [('SA-HP', 'train'), ('SA-HP', 'val')]
+data_sets  = [('SA-HP', 'train'), ('SA-HP', 'val')]
 classes, _      = get_classes(classes_path)
 
-photo_nums  = np.zeros(len(VOCdevkit_sets))
+photo_nums  = np.zeros(len(data_sets))
 nums        = np.zeros(len(classes))
 def convert_annotation(pathname, image_id, list_file):
-    in_file = open(os.path.join(VOCdevkit_path, '%s/Annotations/%s.xml'%(pathname, image_id)), encoding='utf-8')
+    in_file = open(os.path.join(data_path, '%s/Annotations/%s.xml'%(pathname, image_id)), encoding='utf-8')
     tree=ET.parse(in_file)
     root = tree.getroot()
 
@@ -40,13 +40,13 @@ def convert_annotation(pathname, image_id, list_file):
         
 if __name__ == "__main__":
     random.seed(0)
-    if " " in os.path.abspath(VOCdevkit_path):
+    if " " in os.path.abspath(data_path):
         raise ValueError("数据集存放的文件夹路径与图片名称中不可以存在空格，否则会影响正常的模型训练，请注意修改。")
 
     if annotation_mode == 0 or annotation_mode == 1:
         print("Generate txt in ImageSets.")
-        xmlfilepath     = os.path.join(VOCdevkit_path, 'SA-HP/Annotations')
-        saveBasePath    = os.path.join(VOCdevkit_path, 'SA-HP/ImageSets')
+        xmlfilepath     = os.path.join(data_path, 'SA-HP/Annotations')
+        saveBasePath    = os.path.join(data_path, 'SA-HP/ImageSets')
         temp_xml        = os.listdir(xmlfilepath)
         total_xml       = []
         for xml in temp_xml:
@@ -87,11 +87,11 @@ if __name__ == "__main__":
     if annotation_mode == 0 or annotation_mode == 2:
         print("Generate train.txt and val.txt for train.")
         type_index = 0
-        for pathname, image_set in VOCdevkit_sets:
-            image_ids = open(os.path.join(VOCdevkit_path, '%s/ImageSets/%s.txt'%(pathname, image_set)), encoding='utf-8').read().strip().split('\n')
+        for pathname, image_set in data_sets:
+            image_ids = open(os.path.join(data_path, '%s/ImageSets/%s.txt'%(pathname, image_set)), encoding='utf-8').read().strip().split('\n')
             list_file = open('%s_%s.txt'%(pathname, image_set), 'w', encoding='utf-8')
             for image_id in image_ids:
-                list_file.write('%s/%s/JPEGImages/%s.jpg'%(os.path.abspath(VOCdevkit_path), pathname, image_id))
+                list_file.write('%s/%s/JPEGImages/%s.jpg'%(os.path.abspath(data_path), pathname, image_id))
 
                 convert_annotation(pathname, image_id, list_file)
                 list_file.write('\n')
